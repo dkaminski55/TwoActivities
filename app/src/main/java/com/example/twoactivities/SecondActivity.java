ab0384bfe6e658e24884119ce7dba2937f980e5a
Maya Mercedes <mercedes@Mayas-MacBook-Pro.local>
2020-02-21 21:02:14 +0000

    Initial commit

---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "KEY_TO_REPLY_MESSAGE";
+    private EditText mReply;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String replyMessage = mReply.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY, replyMessage);
+        setResult(RESULT_OK,intent);
+        finish();
+        Log.d(LOG_TAG, "End SecondActivity");
+    }
+}
